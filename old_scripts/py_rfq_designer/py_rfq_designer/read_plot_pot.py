more work on field calculation and correct boundaries from vanes. Cave: to calculate potential, now a new cython module is needed. Need to incorporate this into the rfq class or dans_pymodules. -DW

old_scripts/py_rfq_designer/py_rfq_designer/read_plot_pot.py
@@ -3,6 +3,7 @@ import matplotlib.pyplot as plt
 import pickle
 from scipy.interpolate import interp1d
 from dans_pymodules import FileDialog
+import numpy.ma as ma
 
 
 def smooth(x, window_len=11, window='hanning'):
@@ -70,23 +71,147 @@ if fn is None:
     print(fn)
 
 with open(fn, "rb") as infile:
-    mypot = pickle.load(infile)
+    mydata = pickle.load(infile)
+
+mypot = mydata["phi"]
+mymask = mydata["mask"]
 
 print(mypot.shape)
 
-plt.imshow(mypot[:, :, 350].T, extent=(-0.02, 0.02, -0.015, 0.015))
+z_idx = 50
+_pot = mypot[:, :, z_idx].T
+_msk = mymask[:, :, z_idx].T
+
+masked_pot = ma.masked_array(_pot, mask=_msk)
+
+plt.imshow(masked_pot, extent=(-0.02, 0.02, -0.02, 0.02))
 plt.xlabel("x (m)")
 plt.ylabel("y (m)")
 plt.title("Potential (V)")
 plt.colorbar()
 plt.show()
 
-plt.imshow(mypot[10, :, :], extent=(-0.1, 1.35, -0.02, 0.02))
+x_idx = 15
+
+_pot = mypot[x_idx, :, 534:]
+_msk = mymask[x_idx, :, 534:]
+
+masked_pot = ma.masked_array(_pot, mask=_msk)
+
+# plt.imshow(masked_pot, extent=(-0.1, 1.35, -0.02, 0.02))
+plt.imshow(masked_pot, extent=(1.0, 1.35, -0.02, 0.02))
 plt.xlabel("z (m)")
 plt.ylabel("y (m)")
 plt.title("Potential (V)")
 plt.colorbar()
 plt.show()
+
+# Try masked array for numpy gradient
+masked_pot = ma.masked_array(mypot, mask=mymask)
+
+# Regulargradient, masking after
+ex, ey, ez = np.gradient(mypot, 0.002)
+ex = ma.masked_array(ex, mask=mymask)
+ey = ma.masked_array(ey, mask=mymask)
+ez = ma.masked_array(ez, mask=mymask)
+
+# gradient of masked potential
+exm, eym, ezm = np.gradient(masked_pot, 0.002)
+
+# Ex: Slice and reduce plot size for visibility
+_ex = ex[x_idx, :, 534:]
+_exm = exm[x_idx, :, 534:]
+_ex_1d = ex[x_idx, x_idx, 534:]
+_exm_1d = exm[x_idx, x_idx, 534:]
+
+fig, ax = plt.subplots(2, 1)
+p1, p2 = ax
+
+plt.sca(p1)
+plt.imshow(_ex, extent=(1.0, 1.35, -0.02, 0.02))
+plt.xlabel("z (m)")
+plt.ylabel("y (m)")
+plt.title("Ex (V/m)")
+plt.colorbar()
+plt.twinx()
+plt.ylabel("Ex(z) (V/m)")
+plt.plot(np.linspace(1.0, 1.35, 192), _ex_1d)
+
+plt.sca(p2)
+plt.imshow(_exm, extent=(1.0, 1.35, -0.02, 0.02))
+plt.xlabel("z (m)")
+plt.ylabel("y (m)")
+plt.title("Ex (V/m)")
+plt.colorbar()
+plt.twinx()
+plt.ylabel("Ex(z) (V/m)")
+plt.plot(np.linspace(1.0, 1.35, 192), _exm_1d)
+
+plt.show()
+
+# Ex: Slice and reduce plot size for visibility
+_ey = ey[x_idx, :, 534:]
+_eym = eym[x_idx, :, 534:]
+_ey_1d = ey[x_idx, x_idx, 534:]
+_eym_1d = eym[x_idx, x_idx, 534:]
+
+fig, ax = plt.subplots(2, 1)
+p1, p2 = ax
+
+plt.sca(p1)
+plt.imshow(_ey, extent=(1.0, 1.35, -0.02, 0.02))
+plt.xlabel("z (m)")
+plt.ylabel("y (m)")
+plt.title("Ey (V/m)")
+plt.colorbar()
+plt.twinx()
+plt.ylabel("Ey(z) (V/m)")
+plt.plot(np.linspace(1.0, 1.35, 192), _ey_1d)
+
+plt.sca(p2)
+plt.imshow(_eym, extent=(1.0, 1.35, -0.02, 0.02))
+plt.xlabel("z (m)")
+plt.ylabel("y (m)")
+plt.title("Ey (V/m)")
+plt.colorbar()
+plt.twinx()
+plt.ylabel("Ey(z) (V/m)")
+plt.plot(np.linspace(1.0, 1.35, 192), _eym_1d)
+
+plt.show()
+
+# Ez: Slice and reduce plot size for visibility
+_ez = ez[x_idx, :, 534:]
+_ezm = ezm[x_idx, :, 534:]
+_ez_1d = ez[x_idx, x_idx, 534:]
+_ezm_1d = ezm[x_idx, x_idx, 534:]
+
+fig, ax = plt.subplots(2, 1)
+p1, p2 = ax
+
+plt.sca(p1)
+plt.imshow(_ez, extent=(1.0, 1.35, -0.02, 0.02))
+plt.xlabel("z (m)")
+plt.ylabel("y (m)")
+plt.title("Ez (V/m)")
+plt.colorbar()
+plt.twinx()
+plt.ylabel("Ez(z) (V/m)")
+plt.plot(np.linspace(1.0, 1.35, 192), _ez_1d)
+
+plt.sca(p2)
+plt.imshow(_ezm, extent=(1.0, 1.35, -0.02, 0.02))
+plt.xlabel("z (m)")
+plt.ylabel("y (m)")
+plt.title("Ez (V/m)")
+plt.colorbar()
+plt.twinx()
+plt.ylabel("Ez(z) (V/m)")
+plt.plot(np.linspace(1.0, 1.35, 192), _ezm_1d)
+
+plt.show()
+
+exit()
 
 pot_xy0 = mypot[10, 10, :]
 pot_xy0 = smooth(pot_xy0)[5:-5]